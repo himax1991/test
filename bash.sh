81040fe2e77dbe9b282df3178c63a1df89cf8572
himax <himax1991@gmail.com>
2025-01-31 13:37:12 +0300

    werf dev mode

--- bash.sh
+++
@@ -1,9 +0,0 @@
-tee ci/.helm/templates/configmap.yaml << EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: release-channels-data
-data:
-  channels.yaml: |
-{{ $.Files.Get "channels.yaml" | indent 4 }}
-EOF
